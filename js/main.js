181163a358240448a010c2693aa74ced35b81440
Dongwook3 <d_shin2@fanshaweonline.ca>
2017-03-03 19:33:50 -0500

    Add files via upload
    modified main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 $(document).foundation();
+
+(function(){
+	$('.thumbimg img').on('click', function () {
+		$.getJSON('admin/phpscripts/ajaxQuery.php',{ photo:this.id}, function(data){
+			//console.log(data);
+			
+			$('.imageName').text(data.gallery_title);
+			$('.photoGallery img').attr('src', 'images/gallerypics/'+ data.gallery_img);
+			$('.photoDesc p').text(data.gallery_desc);
+			$('.photoCred').text('Photo by: '+data.gallery_photographer);
+		});
+	});
+
+	$fade = $('.thumbimg');
+	
+	$fade.click(function(){
+		$(this).fadeTo(1,0.5);
+		$fade.not($(this)).fadeTo(1,1);
+	});
+
+})();
